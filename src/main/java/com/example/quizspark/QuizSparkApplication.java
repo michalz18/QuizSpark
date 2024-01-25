9fac6c092b94df3c8827a9d561045a47dad80bc1
Michał Zuzak <michalzuzak.2@gmail.com>
2024-01-25 18:55:10 +0100

    Initial commit

---
+++ src/main/java/com/example/quizspark/QuizSparkApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizSparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizSparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quizspark/QuizSparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizspark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizSparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
