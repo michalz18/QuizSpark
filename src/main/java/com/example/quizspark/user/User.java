640a72adad2dc0d105d4c9348b29f8ce21ee8b6a
Michał Zuzak <michalzuzak.2@gmail.com>
2024-01-29 22:01:08 +0100

    [ADD] user entity

---
+++ src/main/java/com/example/quizspark/user/User.java
@@ -0,0 +1,28 @@
+package com.example.quizspark.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+}
