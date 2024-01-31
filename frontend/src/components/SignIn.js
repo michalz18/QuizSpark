ad29f8cc73318cffeca77063920d1d820ffec62c
Michał Zuzak <michalzuzak.2@gmail.com>
2024-02-01 00:27:41 +0100

    [ADD] sign up component and linked to sing in (without backend connection)

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -12,6 +12,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Link as RouterLink } from 'react-router-dom';
 
 const defaultTheme = createTheme();
 
@@ -78,7 +79,7 @@ export default function SignIn() {
                         </Button>
                         <Grid container>
                             <Grid item>
-                                <Link href="#" variant="body2">
+                                <Link component={RouterLink} to="/signup" href="#" variant="body2">
                                     {"Don't have an account? Sign Up"}
                                 </Link>
                             </Grid>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignIn from './components/SignIn';
+import SignUp from "./components/SignUp";
 
 const AppRouter = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
             </Routes>
         </Router>
     );
