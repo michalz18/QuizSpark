8f0bd6e8fa108275d236b9ffc9bfe4ce64ee8960
Michał Zuzak <michalzuzak.2@gmail.com>
2024-02-02 04:30:48 +0100

    [ADD] validations for question entity

--- src/main/java/com/example/quizspark/quizzes/service/QuizServiceImpl.java
+++ src/main/java/com/example/quizspark/quizzes/service/QuizServiceImpl.java
@@ -1,13 +1,44 @@
 package com.example.quizspark.quizzes.service;
 
 import com.example.quizspark.quizzes.model.Quiz;
+import com.example.quizspark.quizzes.repository.QuizRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface QuizServiceImpl {
+@Service
+public class QuizServiceImpl implements QuizService {
 
-    Quiz saveQuiz(Quiz quiz);
-    Quiz getQuiz(UUID id);
-    List<Quiz> getQuizzes();
+    private final QuizRepository quizRepository;
+
+    @Autowired
+    public QuizServiceImpl(QuizRepository quizRepository) {
+        this.quizRepository = quizRepository;
+    }
+
+    @Override
+    @Transactional
+    public Quiz saveQuiz(Quiz quiz) {
+        quiz.getQuestions().forEach(question -> {
+            question.setQuiz(quiz);
+            question.getAnswers().forEach(answer -> answer.setQuestion(question));
+        });
+        return quizRepository.save(quiz);
+    }
+
+    @Override
+    public Quiz getQuizById(UUID id) {
+        return quizRepository.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Quiz not found for id: " + id));
+    }
+
+    @Override
+    public List<Quiz> getAllQuizzes() {
+        return quizRepository.findAll();
+    }
 }
