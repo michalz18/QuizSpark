64ef59a91d7afeaf347ddb5b5b8ca8049e23ef37
Michał Zuzak <michalzuzak.2@gmail.com>
2024-02-02 04:29:21 +0100

    [ADD] validations for quiz entity

--- src/main/java/com/example/quizspark/quizzes/model/Quiz.java
+++ src/main/java/com/example/quizspark/quizzes/model/Quiz.java
@@ -1,8 +1,12 @@
-package com.example.quiz.model;
+package com.example.quizspark.quizzes.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Getter
@@ -10,8 +14,10 @@ import lombok.Setter;
 public class Quiz {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotBlank(message = "Title must not be blank")
     private String title;
 
     @OneToMany(mappedBy = "quiz", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
