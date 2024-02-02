d097bfb8e9b97087a79c87a18b9eb949b543a976
Michał Zuzak <michalzuzak.2@gmail.com>
2024-02-02 02:01:06 +0100

    [ADD] menu app bar

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignIn from './components/SignIn';
 import SignUp from "./components/SignUp";
-import Dashboard from "./components/Dashboard";
+import DashboardAppBar from "./components/DashboardAppBar";
 import { AuthProvider } from './context/AuthContext';
 
 const AppRouter = () => {
@@ -12,7 +12,7 @@ const AppRouter = () => {
                 <Routes>
                     <Route path="/" element={<SignIn />} />
                     <Route path="/signup" element={<SignUp />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/dashboard" element={<DashboardAppBar />} />
                 </Routes>
             </Router>
         </AuthProvider>
