74c2aa3e59794861220f1f916e7e1a5432eaf35e
Michał Zuzak <michalzuzak.2@gmail.com>
2024-02-04 15:31:12 +0100

    [ADD] documentation quiz DTO's

--- src/main/java/com/example/quizspark/quizzes/dto/QuestionDTO.java
+++ src/main/java/com/example/quizspark/quizzes/dto/QuestionDTO.java
@@ -6,10 +6,24 @@ import lombok.Setter;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Data Transfer Object for question details within a quiz.
+ */
 @Getter
 @Setter
 public class QuestionDTO {
+    /**
+     * Unique identifier for the question.
+     */
     private UUID id;
+
+    /**
+     * Content of the question.
+     */
     private String questionContent;
+
+    /**
+     * List of possible answers for this question.
+     */
     private List<AnswerDTO> answers;
 }
--- src/main/java/com/example/quizspark/quizzes/dto/ResultDTO.java
+++ src/main/java/com/example/quizspark/quizzes/dto/ResultDTO.java
@@ -4,11 +4,25 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Data Transfer Object representing the result of a quiz attempt.
+ */
 @Getter
 @Setter
 @Builder
 public class ResultDTO {
+    /**
+     * The total number of questions in the quiz.
+     */
     private int totalQuestions;
+
+    /**
+     * The number of correctly answered questions.
+     */
     private int correctAnswers;
+
+    /**
+     * The calculated score as a percentage of correct answers.
+     */
     private double score;
 }
