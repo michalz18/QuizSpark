980c74ff721dbbd02b4b2bd9d915a4f60ad428fa
Michał Zuzak <michalzuzak.2@gmail.com>
2024-02-04 11:48:47 +0100

    [ADD] documentation for requests within security package

--- src/main/java/com/example/quizspark/security/authentication/requests/RegisterRequest.java
+++ src/main/java/com/example/quizspark/security/authentication/requests/RegisterRequest.java
@@ -5,15 +5,38 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Encapsulates the data required for registering a new user in the system.
+ * Includes personal information, credentials, and the desired role for the new account.
+ */
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
 
+    /**
+     * The first name of the user attempting to register.
+     */
     private String firstname;
+
+    /**
+     * The last name of the user attempting to register.
+     */
     private String lastname;
+
+    /**
+     * The email address of the user, which will be used as the username for the account.
+     */
     private String email;
+
+    /**
+     * The password chosen by the user for the new account.
+     */
     private String password;
+
+    /**
+     * The role the user is requesting. Depending on application policies, this may require approval.
+     */
     private String role;
 }
