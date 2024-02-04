efe614bd730c40737756413f2710b5bb1e8fb392
Michał Zuzak <michalzuzak.2@gmail.com>
2024-02-04 15:36:47 +0100

    [ADD] user DTO documentation

--- src/main/java/com/example/quizspark/admin/dto/UserDTO.java
+++ src/main/java/com/example/quizspark/admin/dto/UserDTO.java
@@ -7,14 +7,36 @@ import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
+/**
+ * Data Transfer Object (DTO) representing user information in the QuizSpark application.
+ * This DTO is used for transferring user data between the server and client, particularly in admin operations.
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
+    /**
+     * Unique identifier for the user.
+     */
     private UUID id;
+
+    /**
+     * First name of the user.
+     */
     private String firstname;
+
+    /**
+     * Last name of the user.
+     */
     private String lastname;
+
+    /**
+     * Email address of the user. This can also serve as the username for authentication purposes.
+     */
     private String email;
+
+    /**
+     * Role of the user, indicating their level of access and permissions within the application.
+     */
     private Role role;
 }
-
